e0a9a62205d3b91697def2bbc4346796ad553ec8
Adam <adam.holo@mail.ru>
2021-03-12 15:07:18 +0400

    feat: added functionality min max avg function

---
+++ src/index.js
@@ -0,0 +1,40 @@
+exports.min = function min (array) {
+  if(!array || array.length === 0){
+    return 0;
+  }
+
+  let result = array[0];
+  for(let i = 1; i < array.length; i++){
+    if(result > array[i]){
+      result = array[i];
+    }
+  }
+  return result;
+}
+
+exports.max = function max (array) {
+  if(!array || array.length === 0){
+    return 0;
+  }
+
+  let result = array[0];
+  for(let i = 1; i < array.length; i++){
+    if(result < array[i]){
+      result = array[i];
+    }
+  }
+  return result;
+}
+
+exports.avg = function avg (array) {
+  if(!array){
+    return 0;
+  }
+  let result = 0;
+  let count = 0;
+  for (let i = 0; i < array.length; i++){
+      result += array[i];
+      count++;
+  }
+  return count > 0 ? result / count : 0;
+}
